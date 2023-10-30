7b35dcf3c6fc2b6c1bb3b5683d536686ac5daf9b
GianniMS <1008999@hr.nl>
2023-10-30 18:18:26 +0100

    Added full screen background

--- src/game.ts
+++ src/game.ts
@@ -1,42 +1,37 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
 
 // Import images
-import BackImg from './images/bg1.png';
-
-//Import class
-import { Background } from './Objects/background';
+import BackgroundImg from "./images/bg1.png";
 
 export class Game {
-    //Globals
-    public pixiWidth = 800;
-    public pixiHeight = 450;
+  private pixi: PIXI.Application;
+  private loader: PIXI.Loader;
 
-    private pixi : PIXI.Application;
-    private loader : PIXI.Loader;
+  constructor() {
+    // Create PIXI Stage
+    this.pixi = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    this.pixi.stage.interactive = true;
+    this.pixi.stage.hitArea = this.pixi.renderer.screen;
+    document.body.appendChild(this.pixi.view);
 
-    private background : Background;
+    // Create Loader
+    this.loader = new PIXI.Loader();
+    this.loader.add("backgroundTexture", BackgroundImg);
+    this.loader.load(() => this.loadCompleted());
+  }
 
-    constructor (){
-        // Create PIXI Stage
-        this.pixi = new PIXI.Application({width: this.pixiWidth, height: this.pixiHeight});
-        this.pixi.stage.interactive = true;
-        this.pixi.stage.hitArea = this.pixi.renderer.screen;
-        document.body.appendChild(this.pixi.view);
-
-        // Create Loader
-        this.loader = new PIXI.Loader();
-        this.loader
-            .add('groundTexture', BackImg)
-        this.loader.load(()=>this.loadCompleted());
-    }
-
-    private loadCompleted(){
-        // Adding background to game
-        this.background = new Background(this.loader.resources["backgroundTexture"].texture!, this.pixiHeight, this.pixiWidth);
-        this.pixi.stage.addChild(this.background);
-        
-        // Update
-    }
+  private loadCompleted() {
+    const background = new PIXI.Sprite(
+      this.loader.resources["backgroundTexture"].texture
+    );
+    background.width = window.innerWidth;
+    background.height = window.innerHeight;
+    this.pixi.stage.addChild(background);
+    // Update
+  }
 }
 
 new Game();
